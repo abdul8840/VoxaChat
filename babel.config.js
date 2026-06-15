b9b6bc66ffc817d71cc3950f3ea268aaec55ac59
abdul8840 <abdul14941naseer@gmail.com>
2026-06-15 21:21:45 +0530

    project setup

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,39 @@
 module.exports = {
   presets: ['module:@react-native/babel-preset'],
+  plugins: [
+    [
+      'module-resolver',
+      {
+        root: ['./src'],
+        extensions: [
+          '.ios.js',
+          '.android.js',
+          '.js',
+          '.jsx',
+          '.ts',
+          '.tsx',
+          '.json',
+        ],
+        alias: {
+          '@components': './src/components',
+          '@screens': './src/screens',
+          '@navigation': './src/navigation',
+          '@services': './src/services',
+          '@hooks': './src/hooks',
+          '@redux': './src/redux',
+          '@utils': './src/utils',
+          '@theme': './src/theme',
+        },
+      },
+    ],
+    [
+      'module:react-native-dotenv',
+      {
+        moduleName: '@env',
+        path: '.env',
+        safe: false,
+      },
+    ],
+    'react-native-reanimated/plugin',
+  ],
 };
